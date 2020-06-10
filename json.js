41295c0ac714e1504d6b3e517652918a1d4070d5
Emir Kotan <emirkotan@gmail.com>
2020-06-10 17:43:29 +0100

    json.js fetches properties

--- json.js
+++ json.js
@@ -1,11 +1,29 @@
 const { readFileSync } = require("fs")
+
 const { fd } = process.stdin
-const str = readFileSync(fd, { encoding: "utf8" })
+const args = process.argv.slice(2)
+
+const input = readFileSync(fd, { encoding: "utf8" })
+
 function eol(str) {
     return str + (str.endsWith("\n") ? "" : "\n")
 }
+
 try {
-    process.stdout.write(eol(JSON.stringify(JSON.parse(str), null, 2)))
+    let x = JSON.parse(input)
+
+    for (const a of args)
+        if (x !== undefined && x !== null)
+            x = x[a]
+
+    if (typeof x === "object")
+        x = JSON.stringify(x, null, 2)
+
+    else
+        x = String(x)
+
+    process.stdout.write(eol(x))
+
 } catch (e) {
-    process.stderr.write(eol(e.toString()))
+    process.stdout.write(eol(input))
 }
