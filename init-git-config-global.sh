fe1267d8ffdf1d71ad626e23b265ff84f06e7a67
Emir Kotan <emirkotan@gmail.com>
2021-07-26 17:58:15 +0100

    init-git-config-global.sh

---
+++ init-git-config-global.sh
@@ -0,0 +1,9 @@
+git config --global user.email emirkotan@gmail.com
+git config --global user.name "Emir Kotan"
+git config --global pull.ff only
+git config --global push.default upstream
+git config --global merge.tool vimdiff
+git config --global merge.conflictstype diff3
+git config --global mergetool.prompt false
+git config --global mergetool.keepbackup false
+git config --global credential.helper osxkeychain
