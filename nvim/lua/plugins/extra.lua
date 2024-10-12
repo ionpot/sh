22b51f65409a75f8ee66345b67a02eba61858e72
Emir Kotan <emirkotan@gmail.com>
2024-10-12 20:32:16 +0100

    add flutter-tools

--- nvim/lua/plugins/extra.lua
+++ nvim/lua/plugins/extra.lua
@@ -169,4 +169,13 @@ return {
 			{ "<leader>gs", vim.cmd.Git },
 		},
 	},
+	{
+		"akinsho/flutter-tools.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+		config = true,
+	},
 }
