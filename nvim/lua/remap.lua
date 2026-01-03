bf950c8528340b91788725b9d1920a074504c0d5
Emir Kotan <emirkotan@gmail.com>
2026-01-03 19:17:08 +0000

    <leader>ru runs current line as shell command

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,10 +1,15 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ru", function ()
+  vim.cmd("put=system(getline('.'))")
+end)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- get buffer contents, bufnr=0 for current buffer
+-- vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
 --vim.keymap.set("n", "J", "mzJ`z")
 --vim.keymap.set("n", "<C-d>", "<C-d>zz")
