050ca9a328edfbd0998f3882e074d7debafa200c
Emir Kotan <emirkotan@gmail.com>
2026-01-03 19:15:19 +0000

    [d and ]d remaps no longer needed (now native)

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -49,8 +49,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		local opts = { buffer = args.buf, remap = false }
 		vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
 		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
