c8d7deae94e21f709c08356a97036c2ed2ce5f75
Emir Kotan <emirkotan@gmail.com>
2026-07-29 16:24:51 +0100

    nvim: remove dressing.nvim

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -67,10 +67,6 @@ return {
 	-- So for api plugins like devicons, we can always set lazy=true
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 
-	-- you can use the VeryLazy event for things that can
-	-- load later and are not important for the initial UI
-	{ "stevearc/dressing.nvim", event = "VeryLazy" },
-
 	{
 		"monaqa/dial.nvim",
 		-- lazy-load on keys
