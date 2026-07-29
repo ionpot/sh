f752ace2471a17b593e2b8a3c2195b2e750f4664
Emir Kotan <emirkotan@gmail.com>
2026-07-29 16:06:36 +0100

    nvim: remove unused treesj plugin

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -84,14 +84,6 @@ return {
 	{ "stevearc/dressing.nvim", event = "VeryLazy" },
 
 	{
-		"Wansmer/treesj",
-		keys = {
-			-- { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
-		},
-		opts = { use_default_keymaps = false, max_join_length = 150 },
-	},
-
-	{
 		"monaqa/dial.nvim",
 		-- lazy-load on keys
 		-- mode is `n` by default. For more advanced options, check the section on key mappings
